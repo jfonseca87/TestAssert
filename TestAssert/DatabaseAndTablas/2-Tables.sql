f68751b23430211d8802bc1b722d00518c778aaa
Jorge Luis Fonseca Rivera <jorge.fonseca87@hotmail.com>
2019-04-20 12:26:05 -0500

    Scripts to create database tables and store procedures

---
+++ TestAssert/DatabaseAndTablas/2-Tables.sql
@@ -0,0 +1,74 @@
+﻿USE [TestAssert]
+GO
+/****** Object:  Table [dbo].[Flight]    Script Date: 20/04/2019 12:17:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Flight](
+	[IdFlight] [int] IDENTITY(1,1) NOT NULL,
+	[SourceCity] [varchar](100) NOT NULL,
+	[DestinationCity] [varchar](100) NOT NULL,
+	[DepartureDateTime] [datetime] NOT NULL,
+	[PlaneNumber] [varchar](10) NOT NULL,
+	[PilotName] [varchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IdFlight] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[UserFlight]    Script Date: 20/04/2019 12:17:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[UserFlight](
+	[IdUser] [int] IDENTITY(1,1) NOT NULL,
+	[DocumentType] [varchar](3) NOT NULL,
+	[DocumentNumber] [int] NOT NULL,
+	[UserName] [varchar](100) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[PhoneNumber] [varchar](15) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IdUser] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[UserFlightRegister]    Script Date: 20/04/2019 12:17:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserFlightRegister](
+	[IdUserFlightRegister] [int] IDENTITY(1,1) NOT NULL,
+	[IdUser] [int] NOT NULL,
+	[IdFlight] [int] NOT NULL,
+ CONSTRAINT [PK_UserFlightRegister] PRIMARY KEY CLUSTERED 
+(
+	[IdUserFlightRegister] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[UserFlightRegister]  WITH CHECK ADD  CONSTRAINT [FK__UserFligh__IdFli__145C0A3F] FOREIGN KEY([IdFlight])
+REFERENCES [dbo].[Flight] ([IdFlight])
+GO
+ALTER TABLE [dbo].[UserFlightRegister] CHECK CONSTRAINT [FK__UserFligh__IdFli__145C0A3F]
+GO
+ALTER TABLE [dbo].[UserFlightRegister]  WITH CHECK ADD  CONSTRAINT [FK__UserFligh__IdUse__1367E606] FOREIGN KEY([IdUser])
+REFERENCES [dbo].[UserFlight] ([IdUser])
+GO
+ALTER TABLE [dbo].[UserFlightRegister] CHECK CONSTRAINT [FK__UserFligh__IdUse__1367E606]
+GO
---
+++ TestAssert/DatabaseAndTablas/3-StoreProcedures.sql
@@ -0,0 +1,182 @@
+﻿USE [TestAssert]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_CreateFlight]    Script Date: 20/04/2019 12:22:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Fonseca
+-- Create date: 20/04/2019
+-- Description:	Store procedure to create flights
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_CreateFlight]
+	@SourceCity VARCHAR(100),
+	@DestinationCity VARCHAR(100),
+	@DepartureDateTime DATETIME,
+	@PlaneNumber VARCHAR(10),
+	@PilotName VARCHAR(100),
+	@IdFlight INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO Flight
+	(
+		SourceCity
+		,DestinationCity
+		,DepartureDateTime
+		,PlaneNumber
+		,PilotName
+	) 
+	VALUES
+	(
+		@SourceCity
+		,@DestinationCity
+		,@DepartureDateTime
+		,@PlaneNumber
+		,@PilotName
+	)
+
+	SET @IdFlight = @@IDENTITY
+END
+
+GO
+/****** Object:  StoredProcedure [dbo].[SP_CreateUser]    Script Date: 20/04/2019 12:22:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Fonseca
+-- Create date: 20/04/2019
+-- Description:	Store prcedure to create users
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_CreateUser]
+	@DocumentType VARCHAR(3),
+	@DocumentNumber INT,
+	@UserName VARCHAR(100),
+	@Email VARCHAR(100),
+	@PhoneNumber VARCHAR(15),
+	@IdUser INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO UserFlight
+	(
+		DocumentType
+		,DocumentNumber
+		,UserName
+		,Email
+		,PhoneNumber
+	) 
+	VALUES
+	(
+		@DocumentType
+		,@DocumentNumber
+		,@UserName
+		,@Email
+		,@PhoneNumber
+	)
+
+	SET @IdUser = @@IDENTITY
+END
+
+GO
+/****** Object:  StoredProcedure [dbo].[SP_CreateUserFlightRegister]    Script Date: 20/04/2019 12:22:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Fonseca
+-- Create date: 20/04/2019
+-- Description:	Store procedure to create user-flight records
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_CreateUserFlightRegister]
+	@IdFlight INT,
+	@UserDocumentNumber INT,
+	@IdUserFlightRegister INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @UserId int = 0
+
+	SELECT @UserId = IdUser
+	FROM UserFlight 
+	WHERE DocumentNumber = @UserDocumentNumber
+
+	IF(@UserId > 0)
+	BEGIN
+		INSERT INTO UserFlightRegister
+		(
+			IdUser
+			,IdFlight
+		) 
+		VALUES
+		(
+			@UserId
+			,@IdFlight
+		)
+
+		SET @IdUserFlightRegister = @@IDENTITY
+	END
+END
+
+GO
+/****** Object:  StoredProcedure [dbo].[SP_GetFlight]    Script Date: 20/04/2019 12:22:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Fonseca
+-- Create date: 20/04/2019
+-- Description:	Store procedure to obtain a flight filtered by id
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_GetFlight]
+	@IdFlight INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT	IdFlight
+			,SourceCity
+			,DestinationCity
+			,DepartureDateTime
+			,PlaneNumber
+			,PilotName
+	FROM	Flight
+	WHERE	IdFlight = @IdFlight
+END
+
+GO
+/****** Object:  StoredProcedure [dbo].[SP_GetUserFlight]    Script Date: 20/04/2019 12:22:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Fonseca
+-- Create date: 20/04/2019
+-- Description:	Store procedure to obtain a user filtered by document number
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_GetUserFlight]
+	@DocumentNumber INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT	IdUser
+			,DocumentType
+			,DocumentNumber
+			,UserName
+			,Email
+			,PhoneNumber
+	FROM	UserFlight
+	WHERE	DocumentNumber = @DocumentNumber
+END
+
+GO
